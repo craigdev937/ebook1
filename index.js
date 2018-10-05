bc74908ca43dd1548b2f2548166d7ce77c8d268a
indigomx9 <indigomx9@gmail.com>
2018-10-05 19:11:54 -0400

    Updated the templates.

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const strpe = require('stripe')('sk_test_sqRWr08xH3RuEiwCNZ69y1bP');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Login!' });
+});
 
 const port = process.env.PORT || 5000;  // For Heroku.
 app.listen(port, () => console.log(`Server is running on port ${port}`));
